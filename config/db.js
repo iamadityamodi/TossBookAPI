762c2d684eba1c1e8d36cca4f2b71ddfa8090a6b
Aditya@7589 <adityamodi7589@gmail.com>
2025-11-14 23:17:37 +0530

    fix: remove node_modules from git

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2/promise");
 
 const mySqlPool = mysql.createPool({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: 'Admin@#123',
     database: 'tossbookapi'
