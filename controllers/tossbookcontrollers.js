9c6edf2d823c28bc9f8d001e215ba383782bf699
Aditya@7589 <adityamodi7589@gmail.com>
2025-12-06 22:11:22 +0530

    New Changes

--- controllers/tossbookcontrollers.js
+++ controllers/tossbookcontrollers.js
@@ -819,7 +819,7 @@ const WithdrawalMoney = async (req, res) => {
         console.log(error)
         res.status(500).send({
             success: false,
-            message: 'Error in add coin API',
+            message: 'Error in add coin API Data',
             error
         })
     }
